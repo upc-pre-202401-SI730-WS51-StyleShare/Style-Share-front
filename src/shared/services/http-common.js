7153b8b98c78f2cee37b8511fee614eca72bb21d
christopherMore1 <155859499+christopherMore1@users.noreply.github.com>
2024-05-04 11:40:54 -0500

    Update http-common.js
    cambiada la base url a "http://localhost:3000/"

--- src/shared/services/http-common.js
+++ src/shared/services/http-common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = 'https://my-json-server.typicode.com/upc-pre-202401-SI730-WS51-StyleShare/db-server';
+const API_BASE_URL = 'http://localhost:3000/';
 const http = axios.create({
     baseURL: API_BASE_URL, header:{ //tipo de formate que va a leer la api
         'Content-type': 'application/json',
