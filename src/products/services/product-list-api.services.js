b746437d2e5168349566b6770b95a8c0f4ada811
Joaqquin0 <104875297+Joaqquin0@users.noreply.github.com>
2024-06-28 02:34:04 -0500

    Update product-list-api.services.js

--- src/products/services/product-list-api.services.js
+++ src/products/services/product-list-api.services.js
@@ -2,6 +2,6 @@ import http from "@/shared/services/http-common.js";
 
 export class ProductListApiServices{
     getAll(){
-        return http.get('/products')
+        return http.get('https://my-json-server.typicode.com/upc-pre-202401-SI730-WS51-StyleShare/db-server2/products')
     }
 }
